491b4929c01bec059a943c1072060cd3960b1f98
Sunakshi Thakur <sunakshithakur111@gmail.com>
2026-04-22 09:50:19 +0530

    fix static + remove nested git

--- project1/settings.py
+++ project1/settings.py
@@ -129,7 +129,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 CRISPY_TEMPLATE_PACK = 'bootstrap5'
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 LOGIN_REDIRECT_URL = 'blog-home'
